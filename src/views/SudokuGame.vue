48eee940a3b52c17ca4d3209630012ce66631896
patryk406 <merph406@gmail.com>
2022-04-21 00:15:58 +0200

    added check for empty click, check for winner, updated styles for board. Fixed bug with posibility of clicking first field with the same number in row or col

--- src/views/SudokuGame.vue
+++ src/views/SudokuGame.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 const sudokuNumbers = ref(["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
 const activeNumber = ref("");
 const errors = ref(0);
+const showPopup = ref(false);
+const finalMessage = ref('Congratulations! You won! 😃');
 const board = ref([
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
@@ -14,7 +16,13 @@ const board = ref([
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
 ]);
-
+const checkWinner=()=>{
+  const tempStr = board.value.toString()
+  if (tempStr.includes('-')){
+    return
+  }
+  showPopup.value=true;
+}
 function validateCell(colKey, cellKey, value) {
   let checkTables = { a: false, b: false };
   for (let a = 0; a < board.value.length; a++) {
@@ -26,14 +34,17 @@ function validateCell(colKey, cellKey, value) {
       }
     }
   }
-  if (!checkTables.a && !checkTables.b) {
+  if (!checkTables.a && !checkTables.b && value !=='') {
     board.value[colKey][cellKey] = value;
-  } else errors.value += 1;
+  } else if (value !== '') {
+    errors.value += 1
+  };
+  checkWinner();
 }
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return `${Math.floor(Math.random() * (max - min)) + min}`;
 }
 const arrangeBoard = () => {
   for (let a = 0; a < 18; a++) {
@@ -54,20 +65,22 @@ const resetBoard = () => {
   activeNumber.value = "";
   arrangeBoard();
 };
+arrangeBoard();
 </script>
 
 <template>
-  <main class="pt-8 text-center min-h-screen">
-    <h1 class="mb8 text-3xl font-bold uppercase">Sudoku</h1>
-    <h4>Errors: {{ errors }}</h4>
+  <main class="sudoku_container pt-8 text-center">
+    <h1 class="mb-7 text-3xl font-bold uppercase">Sudoku</h1>
+    <h4  class="font-bold">Errors: <span class="text-red-800 font-bold text-xl ml-1">{{ errors }}</span></h4>
     <div class="board flex items-center justify-center mt-10">
       <div
-        class="row flex flex-col"
+        class="row_col row flex flex-col"
         v-for="(col, colKey) in board"
         :key="colKey"
       >
         <div
-          class="col w-8 h-8 m-1 bg-gray-100 cursor-pointer"
+          class="sudoku_col text-center bg-gray-100 font-bold col w-10 h-10 cursor-pointer hover:bg-gray-400"
+          :class="cell !=='' && cell !=='-' ? 'bg-gray-400' : ''"
           v-for="(cell, cellKey) in col"
           :key="cellKey"
           @click="validateCell(colKey, cellKey, activeNumber)"
@@ -77,26 +90,61 @@ const resetBoard = () => {
       </div>
     </div>
 
-    <div class="numbers flex items-center justify-center flex-col mt-20">
-      <h3 class="text-2xl font-bold">Chose number</h3>
+    <div class="numbers flex items-center justify-center flex-col mt-10">
+      <h3 class="text-2xl font-bold mb-2">Chose number</h3>
       <div class="numbers-container flex">
         <span
           :class="activeNumber === number ? 'active' : ''"
-          class="bg-gray-100 p-2 mr-2 border border-1-solid border-gray-100 cursor-pointer"
+          class="bg-gray-100 text-xl p-5 mr-2 border border-1-solid border-gray-100 cursor-pointer rounded-3xl"
           v-for="(number, id) in sudokuNumbers"
           :key="id"
           @click="activeNumber = number"
           >{{ number }}</span
         >
       </div>
-      <button @click="resetBoard">Reset Board</button>
+      <button @click="resetBoard" class="w-40 h-10 bg-red-800 font-bold text-xl rounded text-white mt-10">Reset Board</button>
+    </div>
+    <div class="popup" v-show="showPopup">
+      <div class="popup_container flex items-center justify-center
+flex-col bg-gray-200 p-10 rounded-2xl border border-4 border-white">
+        <h2 class="final_message text-xl font-semibold">{{ finalMessage }}</h2>
+        <button @click="resetBoard" class="play_button mt-10 p-3 bg-gray-300 rounded-2xl font-semibold hover:bg-gray-500 hover:text-white">Play Again</button>
+      </div>
     </div>
   </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .active {
   background-color: #616161;
   color: white;
+}
+.sudoku_col{
+  border:1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.row_col:nth-of-type(3),
+.row_col:nth-of-type(6){
+  .sudoku_col{
+    border-right:2px solid black;
+  }
+}
+.sudoku_col:nth-of-type(3),
+.sudoku_col:nth-of-type(6){
+  border-bottom:2px solid black;
+}
+.popup{
+  position: absolute;
+  top:0;
+  left:0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  white-space: pre-wrap;
+  width: 100vw;
+  height:100vh;
+  background-color: rgba(0, 0, 0, 0.90);
 }
 </style>
